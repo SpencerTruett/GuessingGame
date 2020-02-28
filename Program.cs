6a4641197de2827726cb913d44e9b11126650110
Spencer Truett <srtruett1993@gmail.com>
2020-02-28 09:32:01 -0600

    added while loop

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 
-namespace GuessingGame {
-    class Program {
-        static void Main (string[] args) {
-            Random rand = new Random ();
-            int secretNumber = rand.Next (1, 11);
+namespace GuessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rand = new Random();
+      int secretNumber = rand.Next(1, 11);
 
-            for (int i = 0; i < 3; i++) {
-                Console.WriteLine ("Guess a number between 1 and 10");
-                string userGuess = Console.ReadLine ();
-                int userGuessInt = int.Parse (userGuess);
+      while (true)
+      {
+        for (int i = 0; i < 3; i++)
+        {
+          Console.WriteLine("Guess a number between 1 and 10");
+          string userGuess = Console.ReadLine();
+          int userGuessInt = int.Parse(userGuess);
 
-                if (userGuessInt == secretNumber) {
-                    Console.WriteLine ($"You guess it! It was {secretNumber}!");
-                    return;
-                } else if (userGuessInt > secretNumber && i != 2) {
-                    Console.WriteLine ("Nope. Too High. Guess Lower.");
-                } else if (userGuessInt < secretNumber && i != 2) {
-                    Console.WriteLine ("Nope. Too Low. Guess Higher.");
-                } else {
-                    Console.WriteLine ($"Game Over! It was {secretNumber}!");
-                }
-            }
+          if (userGuessInt == secretNumber)
+          {
+            Console.WriteLine($"You guess it! It was {secretNumber}!");
+            break;
+          }
+          else if (userGuessInt > secretNumber && i != 2)
+          {
+            Console.WriteLine("Nope. Too High. Guess Lower.");
+          }
+          else if (userGuessInt < secretNumber && i != 2)
+          {
+            Console.WriteLine("Nope. Too Low. Guess Higher.");
+          }
+          else
+          {
+            Console.WriteLine($"Game Over! It was {secretNumber}!");
+          }
         }
+      }
     }
+  }
 }
